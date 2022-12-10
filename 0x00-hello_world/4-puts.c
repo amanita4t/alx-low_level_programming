280facf8e9c783b177f360347612dc4d3fb7e5df
amanita4t <amanueltekalign4@gmail.com>
2022-12-10 05:45:45 -0800

    hi

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - prints a given phrase
+ * return: 0 if exited properly, non=zero otherwise
+ */
+
+int main(void)
+{
+	puts("\"programing is like building a multilingual puzzle");
+	return (0);
+}
