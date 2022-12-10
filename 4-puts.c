9adb8aa12f15ac3a2a6cf81606f83c0a01b0aa2a
amanita4t <amanueltekalign4@gmail.com>
2022-12-10 05:37:31 -0800

    hi

--- 4-puts.c
+++ 4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - prints a given phrase
+ * return: 0 if exited properly, non=zero otherwise
+ */
+
 int main(void)
 {
 	puts("\"programing is like building a multilingual puzzle");
